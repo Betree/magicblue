918c6757ec7b17e31f2431bf657006ac74ea90ec
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2015-11-27 00:11:31 +0100

    Basic MagicBlue control class that allow connecting, turning on / off the light and setting color

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Tested on Linux only. I'll be happy to get your feedback on other platforms !
 
 ## Installation
 ### Manual
+#### Linux
+You must use python 3+
+
 sudo apt-get install libbluetooth-dev
 pip install pybluez
 pip install gattlib
@@ -13,4 +16,26 @@ pip install gattlib
 TODO
 
 ## Usage
-Script must be run as root
+### Using it as a tool
+Script must be run as root. You also need to set your device MAC address in magicblue.py MAGIC_BLUE_MAC constant (this
+will be automatised in a future version)
+
+### Using it as an API
+    from magicblue import MagicBlue
+    
+    bulb_mac_address = 'XX:XX:XX:XX:XX:XX'
+    bulb = MagicBlue(bulb_mac_address)
+    bulb.connect()
+    bulb.set_color([255, 0, 0])         # Set red
+    bulb.set_random_color([255, 0, 0])  # Set random
+    bulb.turn_off()                     # Turn off the light
+    bulb.turn_on()                      # Set white light
+
+## Details
+You can get more details on the protocol by checking the [https://github.com/Betree/pyMagicBlue/wiki](Wiki pages).
+
+I haven't fully retro-engineered the protocol yet so it's not complete but
+[https://github.com/Betree/pyMagicBlue/wiki/Characteristics-list](Characteristics list page) and
+[https://github.com/Betree/pyMagicBlue/wiki/How-to-use-manually-with-Gatttool](How to use manually with Gatttool page)
+should give you enough details to start working on your own implementation if you need to port this for another
+language / platform
--- magicblue.py
+++ magicblue.py
@@ -1,38 +1,102 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# ==============================================================================
+# ========================================================================================
 # title           : magicblue.py
 # description     : Python script and library to control Magic Blue bulbs over Bluetooth
 # author          : Benjamin Piouffle
 # date            : 23/11/2015
 # usage           : python magicblue.py
 # python_version  : 3.4
-# ==============================================================================
+# ========================================================================================
 
+import argparse
 import os
+import random
 import sys
-from bluetooth.ble import DiscoveryService
+import time
+from gattlib import GATTRequester, DiscoveryService
 from sys import platform as _platform
-
 
 __version__ = 0.1
 
+MAGIC_BLUE_MAC = 'C7:17:1D:43:39:03'
+
+# Handles
+HANDLE_CHANGE_COLOR = 0x0c
+
+
+class MagicBlue:
+    def __init__(self, mac_address):
+        self.mac_address = mac_address
+        self.connection = None
+
+    def connect(self):
+        self.connection = GATTRequester(self.mac_address, False)
+        self.connection.connect(True, "random")
+        print('Connected : {}'.format(self.connection.is_connected()))
+
+    def disconnect(self):
+        self.connection.disconnect()
+
+    def set_color(self, rgb_color):
+        """
+        Change bulb's color
+        :param rgb_color: color as a list of 3 values between 0 and 255
+        """
+        self.connection.write_by_handle(HANDLE_CHANGE_COLOR, bytes(bytearray([0x56] + rgb_color)))
+
+    def set_random_color(self):
+        self.set_color([random.randint(1, 255) for i in range(3)])
+
+    def turn_off(self):
+        self.set_color([0, 0, 0])
+
+    def turn_on(self, brightness=1.0):
+        """
+        Set white color on the light
+        :param brightness: a float value between 0.0 and 1.0 defining the brightness
+        """
+        self.set_color([int(255 * brightness) for i in range(3)])
+
+
+def parse_cmd():
+    parser = argparse.ArgumentParser(description='Utility to manage a Magic Blue bulb over Bluetooth LE')
+    parser.add_argument('-t', '--timeout', default=10, help='Number of seconds before devices search timeout')
+    return parser.parse_args()
+
+
+def search_magic_blue(timeout):
+    service = DiscoveryService()
+
+    # Search for Magic Blue
+    print('Searching for Magic Blue bulb...')
+    while timeout >= 0:
+        devices = service.discover(2)
+        # TODO: Find another way to recognize than MAC
+        magic_blue_mac_address = next((address for address, name in devices.items() if address == MAGIC_BLUE_MAC), None)
+        if magic_blue_mac_address is not None:
+            return magic_blue_mac_address
+        time.sleep(1)
+        timeout -= 1
+
+    raise RuntimeError('Magic blue not found !')
+
 
 def main():
-    # Ask sudo on Linux
+    # Ask Root on Linux
     if _platform == "linux" or _platform == "linux2":
         if os.geteuid() != 0:
             print("Script must be run as root")
             args = ['sudo', sys.executable] + sys.argv + [os.environ]
             os.execlpe('sudo', *args)
-        else:
-            print('Ok !')
 
-    service = DiscoveryService()
-    devices = service.discover(2)
+    # cmd_options = parse_cmd()
+    # Get bluetooth interface
+    # mac_address = search_magic_blue(service, cmd_options.timeout)
+    mac_address = MAGIC_BLUE_MAC
 
-    for address, name in devices.items():
-        print("name: {}, address: {}".format(name, address))
+    magic_blue = MagicBlue(mac_address)
+    magic_blue.connect()
     return 0
 
 
